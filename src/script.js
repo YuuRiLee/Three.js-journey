1cd0ee80b5070bc3a7ea12a61f172616f8cf7829
Yuurilee <yurilee9696@gmail.com>
2021-06-29 01:00:54 +0900

    Feat: mesh를 여러개 만들어 그룹 단위로 위치를 변경하도록 수정

--- src/script.js
+++ src/script.js
@@ -4,20 +4,34 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+// Objects
+const group = new THREE.Group()
+group.position.y = 0.5
+group.scale.y = 2
+group.rotation.y = 0.5
+group.rotation.z = 1
+scene.add(group)
 
-mesh.position.set(0.7, - 0.6, 1)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
 
-mesh.scale.set(2, 0.5, 0.5)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = 0
+group.add(cube2)
 
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
-
-scene.add(mesh)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 1.5
+group.add(cube3)
 
 // Sizes
 const sizes = {
@@ -28,7 +42,6 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.lookAt(mesh.position)
 scene.add(camera)
 
 const axesHelper = new THREE.AxesHelper(2)
