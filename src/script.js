8122d1e835492696dc71866d8c5ecdd49598e587
Yuurilee <yurilee9696@gmail.com>
2021-06-29 00:47:17 +0900

    Feat: 카메라를 물체쪽으로 회전하도록 lookAt 추가

--- src/script.js
+++ src/script.js
@@ -28,6 +28,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 const axesHelper = new THREE.AxesHelper(2)
